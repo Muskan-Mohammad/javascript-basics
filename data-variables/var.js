9c666d40fbbd489d1851a22fb1444eca593838d5
Muskan <101416871+Muskan-Mohammad@users.noreply.github.com>
2022-12-12 12:44:24 +0530

    variables-added

---
+++ data-variables/var.js
@@ -0,0 +1,17 @@
+var a = 24
+//console.log(a)  24
+
+
+var firstName = "Muskan";
+var secondName = "Mohammad";
+var city = "Hyderabad";
+var country = "India";
+
+var fullName = firstName + "" + secondName + "" + city + ""+ country
+// MuskanMohammadHyderabadIndia
+
+
+var fullAddress = `${firstName}  ${secondName} , ${city} , ${country}`
+console.log(fullAddress);
+
+//Muskan  Mohammad , Hyderabad , India
