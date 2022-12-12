0ea813c61b8417890b0b806e120b740111bd4c4f
Muskan <101416871+Muskan-Mohammad@users.noreply.github.com>
2022-12-12 12:12:21 +0530

    coersions-added

---
+++ basics/explicit-coersion.js
@@ -0,0 +1,32 @@
+Number('abc')
+//Nan
+
+Number('')
+//0
+
+Number(' ')
+//0
+
+Number(' n')
+//NaN
+
+
+parseInt('123')
+//123
+
+parseInt('123.745749')
+//123
+
+
+parseInt('123ghsa')
+//123
+
+parseInt('bfhdj3682'); 
+// NaN
+
+parseInt('456fdd3435'); 
+// 456
+
+parseFloat('234324542');
+234324542
+//it passes the value as string and return a number
---
+++ basics/implicit-explicit-conversion.js
@@ -0,0 +1,22 @@
+100 == "100" 
+// true  number gets converted to string 
+
+'34' + 35 
+// '3435'
+
+67 - true
+// 66
+
+'24' === String(24)
+// true
+
+Boolean ('');
+//false
+
+Boolean( ' ');
+// with a gap its true
+
+Boolean(2)==true
+//true
+Boolean(2)===true
+//true
