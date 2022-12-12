ff1f25daf50b861afe3c46aefb2086e550288fba
Muskan <101416871+Muskan-Mohammad@users.noreply.github.com>
2022-12-12 12:31:52 +0530

    non-primitive data

---
+++ data-structure/arrays.js
@@ -0,0 +1,9 @@
+ let fruits = [ "Apple" , "Kiwi" , "Chiko" , "Watermelon" , "Jackfruit"]
+//                 0          1          2         3           4            
+//console.log(3) Watermelon
+
+
+
+["Muskan" , 21 , 24==24 , ["sani" , "abhi" ] , parseFloat('432')] 
+// 0         1     2         3                   4
+// Muskan     21    true     ["sani" , "abhi" ] , 432
---
+++ data-structure/object.js
@@ -0,0 +1,15 @@
+let student = {
+"name" : "Muskan",
+"class" : 10,
+"participant": true,
+"friends" : ["Sani" , "Abhi"],
+"degree": {
+"ug": "B.Sc",
+"couse": "Environmental Science",
+"college": "JSS AHER"
+
+}
+
+
+
+}
