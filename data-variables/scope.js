e789e2c9b5797ba71bacc14f4c605810824f36d7
Muskan <101416871+Muskan-Mohammad@users.noreply.github.com>
2022-12-16 09:18:02 +0530

    new-changes

---
+++ data-variables/scope.js
@@ -0,0 +1,28 @@
+var name = "Muskan";
+let designation = "Web-developer";
+
+{
+
+    var name = "Zoya";
+    let designation="Environmentalits"
+
+
+}
+
+// this displays name = Zoya , designation = Web-developer because let is block scope , n var is function scope
+
+
+//the above one is block-scoped
+
+// Here coomes how the object is represented
+
+var myDetails = {
+name : "Muskan",
+age: 21,
+student: true,
+designation:"Developer"
+
+
+}
+
+console.log(myDetails);
---
+++ first-program/index.js
@@ -0,0 +1,21 @@
+var initial = "Muskan";
+let final = "Mohammad";
+
+const city = "Hyderabad";
+
+let friends = [
+    'Sani',
+    'Abhi'
+]
+
+const designation = "Web-developer ,Environmentalits"
+
+
+const myInformation = {
+    fullname : `${initial} ${final}`,
+    city  :city,
+    friends : friends,
+    designation : designation
+}
+
+console.log(myInformation);
