4ff4252c6a143d43a10a6226215fb0b4f655cf63
Muskan-Mohammad <muskanmd2001@gmail.com>
2023-02-08 11:22:22 +0530

    REST-API's

--- 16-expressjs-REST-API/index.js
+++ 16-expressjs-REST-API/index.js
@@ -12,31 +12,35 @@ app.get('/' , (req , res)=>{
 res.send("Hellow to the express js session of REST-API methods.")
 })
 
-//  GET request method
 
-var USER_ENDPOINT = '/food';
+
+const USER_ENDPOINT = '/food';
+
+//  GET request method to get all the food list
 const apiHandler = (req , res) => {
     res.send({success: true})
 };
 app.get(USER_ENDPOINT , apiHandler);
 
-// POST request metho
+// POST request method to create a new food item
+ 
 
-var USER_ENDPOINT = '/food';
 app.post(USER_ENDPOINT , function(req , res){
-
+ res.send({success: true})
 });
 
 
-// PUT request method
-var USER_ENDPOINT = 'food/:id';
-app.put(USER_ENDPOINT , function( req , res){
+// PUT request method to update anf food item
+// 
+app.put(`${USER_ENDPOINT}/:id`, function( req , res){
     const userID = req.params.id;
+    res.json({status: true , id: userID});
 });
 
 
-// DELETE request method
-var USER_ENDPOINT = 'food/:id';
-app.delete(USER_ENDPOINT , function(req , res){
+// DELETE request method to delete any food item from the menu
+
+app.delete(`${USER_ENDPOINT}/:id` , function(req , res){
     const userID = req.params.id;
-})
+    res.json({status: true , id: userID});
+});
